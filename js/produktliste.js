ba5cbaf1fb3b1de2223952954ae11bb76785165d
signe <sist0002@stud.kea.dk>
2025-09-26 13:18:22 +0200

    discount + sold out classes

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,7 +1,7 @@
 const product_list_container = document.querySelector(".category_grid");
 
 
-fetch(`https://kea-alt-del.dk/t7/api/products`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
     .then((response) => response.json())
     .then(data => showProducts(data));
 
@@ -10,18 +10,20 @@ function showProducts(products) {
     console.log(products);
     products.forEach(element => {
     console.log(element);
-     product_list_container.innerHTML +=  `
-     <div class="product_list_container">
-            <a href="produkt.html">
+     product_list_container.innerHTML +=  `<div class="product_list_container">
+     <a href="produkt.html?id=${element.id}">
               <div class="image_container">
                 <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="cap" width="100%" />
-                <div class="sold_out">Sold out</div>
+                <div class="hidden ${element.soldout && "sold_out"}" >Sold out</div>
               </div>
+              </a>
               <h3>${element.productdisplayname}</h3>
               <p class="category">${element.category}</p>
               <p class="price">${element.price} DKK</p>
-              <div class="sale">-50%</div>
-            </a>
+              <div class="hidden ${element.discount && "sale"}">
+              <p>${element.discount} %</p>
+              <p>Now DKK ${Math.round(element.price - element.price * element.discount / 100)}</p>
+              </div>
     </div>`;
     })
 }
